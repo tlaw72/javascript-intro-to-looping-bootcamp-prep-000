7e66629d2160f62df109226240b8f51628e55214
Troy Lawrence <redirect@gmail.com>
2018-02-07 23:13:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop (array) {
   for (let i=0; i<25; i++) {
-      array.unshift(`"I am ${i + 1} strange loop${i===0 ? '' : 's'}!"`);
+      array.unshift(`"I am ${i} strange loop${i===0 ? '' : 's'}."`);
       return array;
   }
 }
